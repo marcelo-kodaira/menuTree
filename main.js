e74ea12dc7c9fc97c19b56a0e6a8fd203e0cb0b7
Marcelo Almeida <marcelo.aallmeida@gmail.com>
2023-06-16 15:02:53 -0300

    Fix: Tests

--- main.js
+++ main.js
@@ -37,39 +37,45 @@ console.assert(JSON.stringify((0, routing_1.routing)("register")) ===
             label: 'Loja',
             link: '/register/store',
             menuSupId: 'register'
+        },
+        {
+            id: 'internalStore',
+            label: 'Loja Interna',
+            link: '/register/store/internal',
+            menuSupId: 'store'
+        },
+        {
+            id: 'externalStore',
+            label: 'Loja Externa',
+            link: '/register/store/external',
+            menuSupId: 'store'
         }
     ]), "Test 2 failed");
 console.log("-------------- Test 2 --------------");
 console.log((0, routing_1.routing)("register"));
-console.assert(JSON.stringify((0, routing_1.routing)("register")) ===
+console.assert(JSON.stringify((0, routing_1.routing)("store")) ===
     JSON.stringify([
-        {
-            id: 'register',
-            label: 'Cadastros',
-            link: '/register',
-            menuSupId: null
-        },
-        {
-            id: 'people',
-            label: 'Pessoa',
-            link: '/register/people',
-            menuSupId: 'register'
-        },
-        {
-            id: 'car',
-            label: 'Carro',
-            link: '/register/car',
-            menuSupId: 'register'
-        },
         {
             id: 'store',
             label: 'Loja',
             link: '/register/store',
             menuSupId: 'register'
+        },
+        {
+            id: 'internalStore',
+            label: 'Loja Interna',
+            link: '/register/store/internal',
+            menuSupId: 'store'
+        },
+        {
+            id: 'externalStore',
+            label: 'Loja Externa',
+            link: '/register/store/external',
+            menuSupId: 'store'
         }
     ]), "Test 3 failed");
 console.log("-------------- Test 3 --------------");
 console.log((0, routing_1.routing)("store"));
-console.assert(JSON.stringify((0, routing_1.routing)("nonValidId")) === JSON.stringify(null), "Test 4 failed");
+console.assert(JSON.stringify((0, routing_1.routing)("nonValidId")) === JSON.stringify([]), "Test 4 failed");
 console.log("-------------- Test 4 --------------");
 console.log((0, routing_1.routing)("nonValidId"));
--- main.ts
+++ main.ts
@@ -20,30 +20,42 @@ console.assert(
   console.assert(
     JSON.stringify(routing("register")) ===
     JSON.stringify([
-    {
-      id: 'register',
-      label: 'Cadastros',
-      link: '/register',
-      menuSupId: null
-    },
-    {
-      id: 'people',
-      label: 'Pessoa',
-      link: '/register/people',
-      menuSupId: 'register'
-    },
-    {
-      id: 'car',
-      label: 'Carro',
-      link: '/register/car',
-      menuSupId: 'register'
-    },
-    {
-      id: 'store',
-      label: 'Loja',
-      link: '/register/store',
-      menuSupId: 'register'
-    }
+      {
+        id: 'register',
+        label: 'Cadastros',
+        link: '/register',
+        menuSupId: null
+      },
+      {
+        id: 'people',
+        label: 'Pessoa',
+        link: '/register/people',
+        menuSupId: 'register'
+      },
+      {
+        id: 'car',
+        label: 'Carro',
+        link: '/register/car',
+        menuSupId: 'register'
+      },
+      {
+        id: 'store',
+        label: 'Loja',
+        link: '/register/store',
+        menuSupId: 'register'
+      },
+      {
+        id: 'internalStore',
+        label: 'Loja Interna',
+        link: '/register/store/internal',
+        menuSupId: 'store'
+      },
+      {
+        id: 'externalStore',
+        label: 'Loja Externa',
+        link: '/register/store/external',
+        menuSupId: 'store'
+      }
     ]),
     "Test 2 failed"
     )
@@ -52,32 +64,26 @@ console.assert(
   console.log(routing("register"))
   
   console.assert(
-    JSON.stringify(routing("register")) ===
+    JSON.stringify(routing("store")) ===
     JSON.stringify([
-    {
-      id: 'register',
-      label: 'Cadastros',
-      link: '/register',
-      menuSupId: null
-    },
-    {
-      id: 'people',
-      label: 'Pessoa',
-      link: '/register/people',
-      menuSupId: 'register'
-    },
-    {
-      id: 'car',
-      label: 'Carro',
-      link: '/register/car',
-      menuSupId: 'register'
-    },
-    {
-      id: 'store',
-      label: 'Loja',
-      link: '/register/store',
-      menuSupId: 'register'
-    }
+      {
+        id: 'store',
+        label: 'Loja',
+        link: '/register/store',
+        menuSupId: 'register'
+      },
+      {
+        id: 'internalStore',
+        label: 'Loja Interna',
+        link: '/register/store/internal',
+        menuSupId: 'store'
+      },
+      {
+        id: 'externalStore',
+        label: 'Loja Externa',
+        link: '/register/store/external',
+        menuSupId: 'store'
+      }
     ]),
     "Test 3 failed"
     )
@@ -86,7 +92,7 @@ console.assert(
   console.log(routing("store"))
   
   console.assert(
-    JSON.stringify(routing("nonValidId")) === JSON.stringify(null),
+    JSON.stringify(routing("nonValidId")) === JSON.stringify([]),
     "Test 4 failed"
     )
   
